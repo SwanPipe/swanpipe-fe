dc066a07d74e7544fa60215dbdffefcf5f133072
andy <andy@arin.net>
2018-11-15 14:39:30 -0800

    a semi-version of the login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {LoginAccountComponent} from "./login-account/login-account.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'login-account', component: LoginAccountComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainnavComponent } from './mainnav/mainnav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { FormsModule } from "@angular/forms";
 import {
   MatToolbarModule,
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
   MatListModule,
-  MatMenuModule
+  MatMenuModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatFormFieldModule,
+  MatInputModule
 } from '@angular/material';
 
 import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppConfigService } from './app-config.service';
+import { LoginComponent } from './login/login.component';
+import { LoginAccountComponent } from './login-account/login-account.component';
 
 const appInitializerFn = (appConfig: AppConfigService) => {
   return () => {
@@ -29,19 +36,26 @@ const appInitializerFn = (appConfig: AppConfigService) => {
 @NgModule({
   declarations: [
     AppComponent,
-    MainnavComponent
+    MainnavComponent,
+    LoginComponent,
+    LoginAccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
     MatMenuModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
     HttpClientModule
   ],
   providers: [
