b6bd2c417822a368763f23146d3bc2c179bf5b1d
andy <andy@arin.net>
2018-11-18 11:09:18 -0800

    got rid of appconfig service test failure

--- src/app/app-config.service.spec.ts
+++ src/app/app-config.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppConfigService } from './app-config.service';
 
 describe('AppConfigService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientModule ],
+    providers: [ HttpClientModule ]
+  }));
 
   it('should be created', () => {
     const service: AppConfigService = TestBed.get(AppConfigService);
