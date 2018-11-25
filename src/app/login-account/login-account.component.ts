f35daf9bd9bbb7d1d3c8151b767b3009c3328099
andy <andy@arin.net>
2018-11-25 04:49:50 -0800

    some logic and stuff

--- src/app/login-account/login-account.component.ts
+++ src/app/login-account/login-account.component.ts
@@ -19,6 +19,9 @@ import { Component, OnInit } from '@angular/core';
 import { AccountInfoService } from "../services/account-info.service";
 import {AccountInfo} from "../models/account-info";
 
+const LAST_SUCCESSFUL_LOGIN = "lastSuccessfulLogin";
+const LAST_FAILED_LOGIN = "lastFailedLogin";
+
 @Component({
   selector: 'app-login-account',
   templateUrl: './login-account.component.html',
@@ -35,6 +38,22 @@ export class LoginAccountComponent implements OnInit {
       .subscribe( accountInfo => {
         this.accountInfo = accountInfo
       });
+  }
+
+  lastSuccessfulLogin() : string {
+    if( this.accountInfo.data && this.accountInfo.data[LAST_SUCCESSFUL_LOGIN] ) {
+      return this.accountInfo.data[ LAST_SUCCESSFUL_LOGIN ];
+    }
+    //else
+    return null;
+  }
+
+  lastFailedLogin() : string {
+    if( this.accountInfo.data && this.accountInfo.data[ LAST_FAILED_LOGIN ] ) {
+      return this.accountInfo.data[ LAST_FAILED_LOGIN ];
+    }
+    //else
+    return null;
   }
 
 }
